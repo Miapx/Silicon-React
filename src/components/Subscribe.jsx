43503156f847df5236db1e317f0bba941d815fe3
Mia <mihangrahn@gmail.com>
2024-10-31 11:20:54 +0100

    Validerat sub-email

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import notifBell from '../assets/images/notification.svg'
 
 const Subscribe = () => {
+
+    const [emailData, setEmailData] = useState({ email: '' })
+    const [errors, setErrors] = useState({})
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setEmailData({...emailData, [name]: value})
+
+        if (value.trim() === '') {
+            setErrors(prevErrors => ({...prevErrors, [name]: 'This field is required'}))
+        } else {
+            setErrors(prevErrors => ({...prevErrors, [name]: ''}))
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newErrors = {}
+            Object.keys(emailData).forEach(field => {
+                if (emailData[field].trim() === '') {
+                    newErrors[field] = 'You need to enter an email to subscribe'
+                }
+            })
+
+            if (Object.keys(newErrors).length > 0) {
+                setErrors(newErrors)
+                return
+            }
+    }
+
   return (
     <section id="subscribe">
         <div className="container"> 
@@ -11,15 +42,23 @@ const Subscribe = () => {
             <div>
                 <h2>Subscribe to our newsletter to stay informed about latest updates</h2>
             </div>
-            <form className="emailtosub" id="emailtosub"> 
+            <form onSubmit={handleSubmit}  id="emailtosub" noValidate> 
+
+                <div className='emailtosub'> 
                 <label className="form-label" htmlFor="email"></label>
                 <i className="fa-thin fa-envelope envelope"></i>
-                <input className="form-input" type="email" name="email" id="sub-email" placeholder="Your email" />
+                <input className="form-input" type="email" name="email" value={emailData.email} onChange={handleChange} id="sub-email" required placeholder="Your email" />
+                
                 <button className="btn-primary">
                     <span>Subscribe</span> 
                 </button>
+                </div>
+
+                <p className='invalid-input'>{errors.email && errors.email}</p>
+
             </form>
         </div>
+
     </section>  
     )
 }
