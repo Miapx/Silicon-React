c182632d168cf11377cf71c9f573693cd6b80a7e
Mia <mihangrahn@gmail.com>
2024-11-01 15:25:21 +0100

    Städat css

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,9 +9,10 @@ const Testimonials = () => {
     const [testimonialItems, setTestimonialItems] = useState([])
 
     const fetchData = async () => {
-        const res = await fetch ('https://win24-assignment.azurewebsites.net/api/testimonials')
+        const res = await fetch ('https://win24-assignment.azurewebsites.net/api/testimonials',)
         const data = await res.json()
         setTestimonialItems(data)
+        // setTestimonialItem [(...data, {starRating: {ratingFour}} )]
 
     }
 
