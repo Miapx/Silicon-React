e38b79e17ebd8ddd08de92c780eaef9a562ca3d2
Mia <mihangrahn@gmail.com>
2024-11-13 10:49:37 +0100

    Mobilebutton funkar

--- src/components/HamMenu.jsx
+++ src/components/HamMenu.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import menu from '../assets/images/menu.svg'
+import { NavLink } from 'react-router-dom'
+
 
 
 const HamMenu = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
   return (
-    <button className="hamburger">
-        <img src={menu} alt="" />
+  <> 
+    <button className="hamburger" onClick={toggleMenu}>
+      <img src={menu} alt="" />
     </button>
+    <div className={`${showMenu ? 'mobile-menu' : 'mobile-menu-hidden'}`}>
+      <NavLink className="nav-link whitetext mobile-link" to="/features">Features</NavLink>
+      <NavLink className="nav-link whitetext mobile-link" to="/contact">Contact</NavLink>
+    </div>
+  </>
   )
 }
 
